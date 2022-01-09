e24136e6d4833d9f06c3b510c07c575b34c2e01c
aeon <jerry73204@gmail>
2022-01-09 14:43:03 +0800

    Test ArcOwned::{flat_map,flatten}

--- tests/arc_owned.rs
+++ tests/arc_owned.rs
@@ -1,3 +1,4 @@
+use indexmap::IndexMap;
 use ownref::{ArcOwnedA, ArcOwnedC};
 
 #[test]
@@ -8,6 +9,30 @@ fn arc_owned_any_owner() {
     let _: ArcOwnedA<[char; 2], _> = ArcOwnedA::downcast_owner(x)
         .map_err(|_| ())
         .expect("unable to downcast");
+}
+
+#[test]
+fn arc_owned_a_iter() {
+    let map: IndexMap<_, _> = [('a', 1), ('b', 2), ('c', 3)].into_iter().collect();
+    let own: ArcOwnedA<IndexMap<char, usize>> = ArcOwnedA::new(map);
+
+    let vec: Vec<ArcOwnedA<_, (&char, &usize)>> = own.clone().flatten().collect();
+    assert_eq!(vec.len(), 3);
+    assert_eq!(*vec[0], (&'a', &1));
+    assert_eq!(*vec[1], (&'b', &2));
+    assert_eq!(*vec[2], (&'c', &3));
+
+    let keys: Vec<ArcOwnedA<_, &char>> = own.clone().flat_map(|map| map.keys()).collect();
+    assert_eq!(keys.len(), 3);
+    assert_eq!(*keys[0], &'a');
+    assert_eq!(*keys[1], &'b');
+    assert_eq!(*keys[2], &'c');
+
+    let values: Vec<ArcOwnedA<_, &usize>> = own.flat_map(|map| map.values()).collect();
+    assert_eq!(values.len(), 3);
+    assert_eq!(*values[0], &1);
+    assert_eq!(*values[1], &2);
+    assert_eq!(*values[2], &3);
 }
 
 #[test]
